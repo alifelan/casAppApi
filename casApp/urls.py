0b7a800ea2c9c5fead7b4e59337dfe54ebb623c2
Ali <alifelanv@gmail.com>
2019-04-28 04:15:35 -0500

    Fix small typo

--- casApp/urls.py
+++ casApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('user/', views.user, name='user'),
     path('login/', views.login, name='login'),
     path('events/', views.get_events, name='events'),
-    path('allEvents/', views.get_all_event, name='all_events'),
+    path('allEvents/', views.get_all_events, name='all_events'),
     path('locations/', views.get_locations, name='locations'),
     path('casas/', views.get_casas, name='casas')
 ]
